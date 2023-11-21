98fef0603cfccb3bec6f43ac295c5fbe79a7f697
Anand S <anandsatheeshk@gmail.com>
2023-11-21 14:57:25 +0530

    Font added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "@fontsource-variable/schibsted-grotesk";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
