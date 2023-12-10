fd825d8c04d24382796c5f1e02d307fe762ac79b
Anand S <anandsatheeshk@gmail.com>
2023-12-10 11:48:13 +0530

    User registration succes component

---
+++ src/components/RegisterSuccess.jsx
@@ -0,0 +1,11 @@
+const RegisterSuccess = () => {
+	return (
+		<section>
+			<p>Thank you for registering!</p>
+			<p>Verification mail has been sent to your email address.</p>
+			<p>Login after verifying your account.</p>
+		</section>
+	);
+};
+
+export default RegisterSuccess;
