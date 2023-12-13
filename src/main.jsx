49a4a7bbefafee80697b354e72fe7e3d02689b3d
Anand S <anandsatheeshk@gmail.com>
2023-12-13 15:37:15 +0530

    Configured global error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
+import { ErrorBoundary } from "react-error-boundary";
+import FallBack from "./components/ErrorBoundary/FallBack.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
+		<ErrorBoundary FallbackComponent={FallBack}>
+			<Provider store={store}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</Provider>
+		</ErrorBoundary>
 	</React.StrictMode>
 );
