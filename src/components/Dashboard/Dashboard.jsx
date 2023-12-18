7ef44d968769b93809d1e7d031a7f1ce1dfd7163
Anand S <anandsatheeshk@gmail.com>
2023-12-18 21:52:47 +0530

    Remove card member components

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+	return (
+		<div className="col-span-10 p-2 mt-3 ml-3">
+			<div className="mb-2 flex justify-between">
+				<h1 className="text-xl font-bold hover:underline">Dashboard</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Dashboard;
