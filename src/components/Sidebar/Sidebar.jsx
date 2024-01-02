3a94ef1d9e8f81b7e835285a1838c53e18eb9963
Anand S <anandsatheeshk@gmail.com>
2024-01-02 18:14:21 +0530

    Responsive sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,20 @@
 import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
 
-const Sidebar = ({ sections }) => {
+const Sidebar = ({ sections, close }) => {
 	return (
-		<div className="hidden">
-			<ul>
+		<div className="fixed sm:static bg-blue-300 h-screen w-screen z-50 sidebar">
+			<button
+				onClick={() => close((state) => !state)}
+				className="font-semibold w-full text-right pr-3 pt-2"
+			>
+				X
+			</button>
+			<ul className="w-full">
 				{sections?.map(({ header, link }) => (
 					<li
 						key={link}
-						className="text-center font-semibold text-md p-2 hover:black"
+						className="text-center font-semibold text-sm p-2 hover:black"
 					>
 						<span className="m-1">
 							<NavLink to={link}>{header}</NavLink>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -106,21 +106,44 @@ const Project = () => {
 		];
 	}
 
+	const [toggle, setToggle] = useState(false);
+
 	return (
 		<ProjectContext.Provider value={{ ...data }}>
 			<div className="flex flex-col h-screen">
 				<Navbar />
 				<div className="flex">
-					<Sidebar sections={sections} />
-					<div className="flex-1 w-full">
-						<h1 className="underline px-3 py-1 font-bold text-lg bg-primary text-white">
-							{projectName ? projectName : "Loading..."}
-						</h1>
+					{toggle && <Sidebar sections={sections} close={setToggle} />}
+					<div className="flex-1 w-full ">
+						<div className="bg-primary flex gap-5 items-center p-1 pl-2">
+							<SideBarToggle toggle={toggle} setToggle={setToggle} />
+							<h1 className="font-semibold text-lg text-white">
+								{projectName ? projectName : "Loading..."}
+							</h1>
+						</div>
 						<Outlet />
 					</div>
 				</div>
 			</div>
 		</ProjectContext.Provider>
+	);
+};
+
+const SideBarToggle = ({ setToggle }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			className="w-4 h-4 sm:w-5 sm:h-5 text-white font-semibold border-2 border-white"
+			onClick={() => setToggle((state) => !state)}
+		>
+			<path
+				fillRule="evenodd"
+				d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+				clipRule="evenodd"
+			/>
+		</svg>
 	);
 };
 
